c5483fc4739da8902d286096602df01d553404fe
yuujin-Q <92687071+Eugene316@users.noreply.github.com>
2023-03-22 01:40:50 +0700

    feat: search-states txt generator for coloring

---
+++ src/MazeColoring.cs
@@ -0,0 +1,106 @@
+using System;
+using System.Drawing;
+using System.Text;
+using System.IO;
+
+namespace Tubes2_zainali
+{
+    public class MazeColoring
+    {
+        private string _filename;
+        private Maze _maze;
+        private List<int[,]> _mazeStateLog;
+        private int[,] _mazeStateTemplate;
+
+        public MazeColoring(Maze maze)
+        {
+            this._filename = (DateTime.Now.ToString() + ".txt").Replace('/', '-').Replace(':', '-').Replace(' ', '_');
+            
+            this._maze = maze;
+            this._mazeStateLog = new List<int[,]>();
+
+            this._mazeStateTemplate = new int[_maze.RowCount, _maze.ColCount];
+            for (int i = 0; i < _maze.RowCount; i++)
+            {
+                for (int j = 0; j < _maze.ColCount; j++)
+                {
+                    if (_maze.GetMazeTile(i, j) == 'X')
+                    {
+                        _mazeStateTemplate[i, j] = -1;
+                    }
+                }
+                
+            }
+        }
+
+        public string FileName
+        {
+            get { return this._filename; }
+        }
+
+        public int[,] GenerateColoringState(string directionsTaken)
+        {
+            int[,] colorState = (int[,])this._mazeStateTemplate.Clone();
+            Point playerPosition = _maze.StartPoint;
+
+            colorState[playerPosition.X, playerPosition.Y]++;
+            foreach (char direction in directionsTaken)
+            {
+                playerPosition = Maze.GetNextPoint(playerPosition, direction);                
+                colorState[playerPosition.X, playerPosition.Y]++;
+            }
+            return colorState;
+        }
+
+        public void BackupColoringState(List<string> directionState)
+        {
+            this._mazeStateLog.Add(this._mazeStateTemplate);
+            foreach (string route in directionState)
+            {
+                int[,] colorState = this.GenerateColoringState(route);
+                this._mazeStateLog.Add(colorState);
+            }
+        }
+
+        public string Log
+        {
+            get {
+                StringBuilder log = new StringBuilder();
+                log.AppendLine(this._filename);
+                log.AppendLine();
+
+                foreach (int[,] mazeColorState in _mazeStateLog)
+                {
+                    log.AppendLine('#'.ToString());
+                    StringBuilder colorState = new StringBuilder();
+                    for (int i = 0; i < _maze.RowCount; i++)
+                    {
+                        for (int j = 0; j < _maze.ColCount; j++)
+                        {
+                            if (mazeColorState[i, j] == -1)
+                            {
+                                colorState.Append('X');
+                            }
+                            else
+                            {
+                                colorState.Append(mazeColorState[i, j]);
+                            }
+                            colorState.Append(' ');
+                        }
+                        colorState.AppendLine();
+                    }
+                    log.AppendLine(colorState.ToString());
+                }
+                return log.ToString();
+            }
+        }
+
+        public void SaveLog(string directory)
+        {
+            string path = Path.Combine(directory, this.FileName);
+            using (StreamWriter writer = new StreamWriter(path)) {
+                writer.WriteLine(this.Log);
+            }
+        }
+    }
+}
